8560ca3407a3029acc4b786462ffb79ac3cac2b0
shawna-p <37048629+shawna-p@users.noreply.github.com>
2020-11-24 13:04:34 -0500

    Tests

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,12 @@ html_theme = 'sphinx_pdj_theme'
 import sphinx_pdj_theme
 html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
+
+# sets the darker appearence
+html_theme_options = {
+    'style': 'darker'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
