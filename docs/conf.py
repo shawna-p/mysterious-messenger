b8740fd32e7ef166b5d3708c34461c7952d2b1bb
shawna-p <37048629+shawna-p@users.noreply.github.com>
2020-11-24 12:58:02 -0500

    More themes

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
+import sphinx_pdj_theme
 sys.path.insert(0, os.path.abspath('..'))
+
 
 
 # -- Project information -----------------------------------------------------
@@ -69,8 +71,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nervproject'
-html_static_path = ['_static']
+html_theme = 'sphinx_pdj_theme'
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
