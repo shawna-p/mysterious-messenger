c02ad60d1e18bb94ef78425b67a6da21c5c4704c
shawna-p <37048629+shawna-p@users.noreply.github.com>
2020-11-24 14:50:30 -0500

    build theme

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ html_static_path = ['_static']
 #import sphinx_pdj_theme
 import sphinx_nervproject_theme
 html_theme = "nervproject"
-html_theme_path = [sphinx_nervproject_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_nervproject_theme.get_html_theme_path()]
 #html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 
