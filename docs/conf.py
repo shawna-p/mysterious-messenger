aaea586a6338d36edad3d59b31e34a99b42f6dc8
shawna-p <37048629+shawna-p@users.noreply.github.com>
2020-11-27 15:41:22 -0500

    Add sidebars

--- docs/conf.py
+++ docs/conf.py
@@ -101,6 +101,8 @@ else:
 #     'style': 'darker'
 # }
 
+html_sidebars = '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
